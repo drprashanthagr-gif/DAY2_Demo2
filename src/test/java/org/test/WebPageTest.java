2771ce0f1c390956d7d3bf9469327d1dfe7db9f2
drprashanthagr_gif <drprashanthagr@gmail.com>
2026-03-12 12:38:36 +0530

    Heloo world

---
+++ src/test/java/org/test/WebPageTest.java
@@ -0,0 +1,37 @@
+package org.test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.testng.annotations.AfterTest;
+
+public class WebPageTest {
+
+    private static WebDriver driver;
+
+    @BeforeTest
+    public void openBrowser() throws InterruptedException {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
+        driver.get("https://drprashanthagr-gif.github.io/DAY2_Demo2/");
+    }
+
+    @Test
+    public void titleValidationTest() {
+        String actualTitle = driver.getTitle();
+        System.out.println("Page Title: " + actualTitle);
+
+        String expectedTitle = "My simple website";
+
+        Assert.assertEquals(actualTitle, expectedTitle, "Page title doesn't match!");
+        Assert.assertTrue(actualTitle.contains("website"));
+    }
+
+    @AfterTest
+    public void closeBrowser() throws InterruptedException {
+        Thread.sleep(2000);
+        driver.quit();
+    }
+}
